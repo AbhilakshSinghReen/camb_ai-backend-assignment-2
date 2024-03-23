bc964fe06d1239ed322b440566f496f1cf1a5fbb
AbhilakshSinghReen <abhilakshreen@gmail.com>
2024-03-23 23:40:56 +0530

    Added prometheus metrics to app and Prom k8s resources.

--- server-and-consumer/src/app.py
+++ server-and-consumer/src/app.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, Response
+import prometheus_client
 
 from .tasks import get_task_progress, long_task, update_task_progress, validate_task_id
 from .utils import generate_task_id
 
 
 app = FastAPI()
+
+add_task_count = prometheus_client.Counter(
+    "add_task_count",
+    "Number of tasks added."
+)
 
 
 @app.post("/api/tasks/add")
@@ -14,6 +20,8 @@ def add_task():
     update_task_progress(new_task_id, 0, 0, status="Queued")
 
     long_task(new_task_id)
+
+    add_task_count.inc(1)
 
     return JSONResponse(status_code=200, content={
         'success': True,
@@ -42,3 +50,11 @@ async def get_task_status(task_id: str):
             'progress': task_progress,
         },
     })
+
+
+@app.get("/metrics")
+async def get_metrics():
+    return Response(
+        content=prometheus_client.generate_latest(),
+        media_type="text/plain",
+    )
