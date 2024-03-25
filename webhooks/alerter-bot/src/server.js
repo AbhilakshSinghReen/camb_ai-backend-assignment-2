f693143f6ea2e869fc50146e957ace462db01cde
AbhilakshSinghReen <abhilakshreen@gmail.com>
2024-03-26 00:02:35 +0530

    Formatted server.js and fixed a small bug.

--- webhooks/alerter-bot/src/server.js
+++ webhooks/alerter-bot/src/server.js
@@ -1,42 +1,42 @@
-// const dotenv = require("dotenv")
-const TelegramBot = require("node-telegram-bot-api")
-const express = require('express');
-const dns = require("node:dns")
-const bodyParser = require('body-parser');
+const dotenv = require("dotenv");
+const TelegramBot = require("node-telegram-bot-api");
+const express = require("express");
+const dns = require("node:dns");
+const bodyParser = require("body-parser");
 
 dns.setDefaultResultOrder("ipv4first");
-// dotenv.config();
+dotenv.config();
 
 const port = process.env.PORT;
 const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN;
 const telegramReceiverUserId = process.env.TELEGRAM_RECEIVER_USER_ID;
 
-const telegramBot = new TelegramBot(telegramBotToken);
+const telegramBot = new TelegramBot(telegramBotToken, { polling: true });
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/webhooks/max-workers-reached', (req, res) => {
+app.post("/webhooks/max-workers-reached", (req, res) => {
   const { scaler, namespace, deployment, numWorkers } = req.query;
 
-  console.log("Received max-workers-reached webhook.")
-  console.log(`    scaler: ${scaler}`)
-  console.log(`    namespace: ${namespace}`)
-  console.log(`    deployment: ${deployment}`)
-  console.log(`    numWorkers: ${numWorkers}`)
+  console.log("Received max-workers-reached webhook.");
+  console.log(`    scaler: ${scaler}`);
+  console.log(`    namespace: ${namespace}`);
+  console.log(`    deployment: ${deployment}`);
+  console.log(`    numWorkers: ${numWorkers}`);
 
   const currentDate = new Date();
   const dateString = currentDate.toISOString();
 
-  let message = ``
-  message += "Your Kubernetes Cluster has reached the max number of workers." + "\n"
-  message += dateString + "\n"
-  message += `Scaler: ${scaler}` + "\n"
-  message += `Deployment: ${namespace}.${deployment}` + "\n"
-  message += `Num Workers: ${numWorkers}`
+  let message = ``;
+  message += "Your Kubernetes Cluster has reached the max number of workers." + "\n";
+  message += dateString + "\n";
+  message += `Scaler: ${scaler}` + "\n";
+  message += `Deployment: ${namespace}.${deployment}` + "\n";
+  message += `Num Workers: ${numWorkers}`;
 
-  telegramBot.sendMessage(telegramReceiverUserId, "foo");
+  telegramBot.sendMessage(telegramReceiverUserId, message);
 
-  res.status(200).send('Webhook received successfully.');
+  res.status(200).send("Webhook received successfully.");
 });
 
 app.listen(port, () => {
